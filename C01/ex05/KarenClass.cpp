f681a4d9689d3481a650a612b0fc98bf5da0ae04
JavierCervilla <javier@cervilla.es>
2022-04-09 00:33:12 +0200

    feat(C01):[+] Karen

--- C01/ex05/KarenClass.cpp
+++ C01/ex05/KarenClass.cpp
@@ -12,35 +12,31 @@
 
 #include "KarenClass.hpp"
 
-Karen::Karen( void ) {
-    std::cout << "Karen instance" << std::endl;
-}
-Karen::~Karen( void ) {
-    std::cout << "Karen destruction" << std::endl;
-}
+Karen::Karen( void ) {}
+Karen::~Karen( void ) {}
 
 void Karen::debug( void ) {
-    std::cout << "Karen debugs." << std::endl;
+    std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger.\nI just love it!" << std::endl;
 }
 
 void Karen::info( void ) {
-    std::cout << "Karen informs." << std::endl;
+    std::cout << "I cannot believe adding extra bacon cost more money.\nYou don't put enough! If you did I would not have to ask for it!" << std::endl;
 }
 
 void Karen::warning( void ) {
-    std::cout << "Karen warns." << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free.\nI've been coming here for years and you just started working here last month." << std::endl;
 }
 
 void Karen::error( void ) {
-    std::cout << "Karen errors." << std::endl;
+    std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
 }
 
 void Karen::complain( std::string level) {
     std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-    void (Karen::*fcts[4])(void) = {&Karen::debug, &Karen::info, &Karen::warning, &Karen::error};
+    void (Karen::*functions[4])(void) = {&Karen::debug, &Karen::info, &Karen::warning, &Karen::error};
     for (int i = 0; i < 4; i++) {
         if (levels[i] == level) {
-            (this->*fcts[i])();
+            (this->*functions[i])();
         }
     }
 }
--- C01/ex06/KarenClass.cpp
+++ C01/ex06/KarenClass.cpp
@@ -12,74 +12,59 @@
 
 #include "KarenClass.hpp"
 
-Karen::Karen(void)
-{
-    std::cout << "Karen instance" << std::endl;
-}
-Karen::~Karen(void)
-{
-    std::cout << "Karen destruction" << std::endl;
+Karen::Karen(void) {}
+Karen::~Karen(void) {}
+
+void Karen::debug( void ) {
+    std::cout << "[ DEBUG ]" << std::endl;
+    std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger.\nI just love it!" << std::endl<< std::endl;
 }
 
-void Karen::debug(void)
-{
-    std::cout << "Karen debugs." << std::endl;
+void Karen::info( void ) {
+    std::cout << "[ INFO ]" << std::endl;
+    std::cout << "I cannot believe adding extra bacon cost more money.\nYou don't put enough! If you did I would not have to ask for it!" << std::endl<< std::endl;
 }
 
-void Karen::info(void)
-{
-    std::cout << "Karen informs." << std::endl;
+void Karen::warning( void ) {
+    std::cout << "[ WARNING ]" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free.\nI've been coming here for years and you just started working here last month." << std::endl << std::endl;
 }
 
-void Karen::warning(void)
-{
-    std::cout << "Karen warns." << std::endl;
+void Karen::error( void ) {
+    std::cout << "[ ERROR ]" << std::endl;
+    std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl<< std::endl;
 }
 
-void Karen::error(void)
-{
-    std::cout << "Karen errors." << std::endl;
-}
 
 void Karen::complain(std::string level)
 {
-    std::cout << "[ " << level << " ]" << std::endl;
     std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-    void (Karen::*fcts[4])(void) = {&Karen::debug, &Karen::info, &Karen::warning, &Karen::error};
+    void (Karen::*functions[4])(void) = {&Karen::debug, &Karen::info, &Karen::warning, &Karen::error};
     int lvl = -1;
-    for (int i = 0; i < 4; i++)
-    {
-        if (levels[i] == level)
+    for (int i = 0; i < 4; i++) {
+        if (levels[i] == level) {
             lvl = i;
+        }
     }
     switch (lvl)
     {
     case 0:
         for (int i = 0; i < 4; i++)
-        {
-            (this->*fcts[i])();
-        }
+            (this->*functions[i])();
         break;
     case 1:
         for (int i = 1; i < 4; i++)
-        {
-            (this->*fcts[i])();
-        }
+            (this->*functions[i])();
         break;
     case 2:
         for (int i = 2; i < 4; i++)
-        {
-            (this->*fcts[i])();
-        }
+            (this->*functions[i])();
         break;
     case 3:
         for (int i = 3; i < 4; i++)
-        {
-            (this->*fcts[i])();
-        }
+            (this->*functions[i])();
         break;
     default:
-        std::cout << "Probably complaining about insignificant problems" << std::endl;
         break;
     }
 }
