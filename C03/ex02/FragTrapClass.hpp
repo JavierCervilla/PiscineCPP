b2aa49cc43bdcaaa4cd43f088b68e24287d210e9
JavierCervilla <javier@cervilla.es>
2022-04-11 01:16:53 +0200

    feat(C03):[+] FragTrap Working, all  with colors

--- C03/ex02/FragTrapClass.hpp
+++ C03/ex02/FragTrapClass.hpp
@@ -1,30 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*   FragTrapClass.hpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jcervill <jcervill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/10 14:28:37 by jcervill          #+#    #+#             */
-/*   Updated: 2022/04/10 14:28:37 by jcervill         ###   ########.fr       */
+/*   Created: 2022/04/11 01:04:19 by jcervill          #+#    #+#             */
+/*   Updated: 2022/04/11 01:04:19 by jcervill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SCAV_TRAP_CLASS_HPP
-# define SCAV_TRAP_CLASS_HPP
+#ifndef FRAG_TRAP_CLASS_HPP
+# define FRAG_TRAP_CLASS_HPP
 # include "ClapTrapClass.hpp"
 
-class ScavTrap: public ClapTrap {
-    private:
-        bool _gateKeeper;
-    public:
-        ScavTrap(void);
-        ScavTrap(std::string name);
-        ScavTrap(ScavTrap const &src);
-        ~ScavTrap(void);
-        ScavTrap &operator=(ScavTrap const &src);
+class FragTrap: public ClapTrap {
 
-        void guardGate( void );
+    public:
+        FragTrap(void);
+        FragTrap(std::string name);
+        FragTrap(FragTrap const &src);
+        ~FragTrap(void);
+        FragTrap &operator=(FragTrap const &src);
+
+        void highFivesGuys( void );
         void getStatus( void );
 };
 
--- C03/ex02/colors.hpp
+++ C03/ex02/colors.hpp
@@ -1,31 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*   colors.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jcervill <jcervill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/10 14:28:37 by jcervill          #+#    #+#             */
-/*   Updated: 2022/04/10 14:28:37 by jcervill         ###   ########.fr       */
+/*   Created: 2022/04/11 00:40:09 by jcervill          #+#    #+#             */
+/*   Updated: 2022/04/11 00:40:09 by jcervill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SCAV_TRAP_CLASS_HPP
-# define SCAV_TRAP_CLASS_HPP
-# include "ClapTrapClass.hpp"
+#ifndef COLORS_HPP
+# define COLORS_HPP
 
-class ScavTrap: public ClapTrap {
-    private:
-        bool _gateKeeper;
-    public:
-        ScavTrap(void);
-        ScavTrap(std::string name);
-        ScavTrap(ScavTrap const &src);
-        ~ScavTrap(void);
-        ScavTrap &operator=(ScavTrap const &src);
+# define RED "\033[0;31m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m"
+# define BLUE "\033[0;34m"
+# define MAGENTA "\033[0;35m"
+# define CYAN "\033[0;36m"
+# define RESET "\033[0m"
 
-        void guardGate( void );
-        void getStatus( void );
-};
+# define BOLD "\033[1m"
+# define UNDERLINE "\033[4m"
+# define REVERSE "\033[7m"
 
 #endif
